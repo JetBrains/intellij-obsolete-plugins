c596203ec9ff45113974db58ecee7b9b2ec423b2
Dmitry Jemerov <yole@jetbrains.com>
2019-05-03 11:21:04 +0200

    Update copyrights

--- struts/src/main/java/com/intellij/struts/StrutsIconProvider.java
+++ struts/src/main/java/com/intellij/struts/StrutsIconProvider.java
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2000-2005 by JetBrains s.r.o. All Rights Reserved.
- * Use is subject to license terms.
+ * Copyright 2000-2019 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package com.intellij.struts;
 
