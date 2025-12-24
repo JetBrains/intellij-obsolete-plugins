9acc92f38c3b4542847e334eb468110966ae8bfc
tanggaowei <tanggaowei@foxmail.com>
2025-12-24 20:24:07 +0800

    Update Play plugin to 2025.3.1 and Java 21

--- play/src/main/java/com/intellij/play/references/PlayPropertyReference.java
+++ play/src/main/java/com/intellij/play/references/PlayPropertyReference.java
@@ -1,52 +1,64 @@
 package com.intellij.play.references;
 
-import com.intellij.codeInspection.LocalQuickFix;
+import com.intellij.lang.properties.IProperty;
 import com.intellij.lang.properties.psi.PropertiesFile;
-import com.intellij.lang.properties.references.CreatePropertyFix;
-import com.intellij.lang.properties.references.PropertyReference;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleUtilCore;
 import com.intellij.play.utils.PlayPathUtils;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
+import com.intellij.psi.*;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
-public final class PlayPropertyReference extends PropertyReference {
+public final class PlayPropertyReference extends PsiReferenceBase<PsiElement> {
+  private final String myKey;
+
   public PlayPropertyReference(@NotNull String value, @NotNull PsiElement element) {
-    super(value, element, null, false);
+    super(element);
+    myKey = value;
   }
 
   @Override
-  protected List<PropertiesFile> getPropertiesFiles() {
-    List<PropertiesFile> propertiesFiles = new ArrayList<>();
+  public PsiElement resolve() {
     final Module module = ModuleUtilCore.findModuleForPsiElement(getElement());
+    if (module == null) {
+      return null;
+    }
 
-    if (module != null) {
-      final Set<PsiDirectory> directories = PlayPathUtils.getConfigDirectories(module);
-      for (PsiDirectory directory : directories) {
-        final PsiFile[] files = directory.getFiles();
-        for (PsiFile file : files) {
-          if (file instanceof  PropertiesFile && file.getName().startsWith("message") ) {
-               propertiesFiles.add((PropertiesFile)file);
+    final Set<com.intellij.psi.PsiDirectory> directories = PlayPathUtils.getConfigDirectories(module);
+    for (com.intellij.psi.PsiDirectory directory : directories) {
+      final PsiFile[] files = directory.getFiles();
+      for (PsiFile file : files) {
+        if (file instanceof PropertiesFile && file.getName().startsWith("message")) {
+          PropertiesFile propertiesFile = (PropertiesFile) file;
+          IProperty property = propertiesFile.findPropertyByKey(myKey);
+          if (property != null) {
+            return property.getPsiElement();
           }
         }
       }
     }
 
-    return propertiesFiles;
+    return null;
   }
 
   @Override
-  public @NotNull LocalQuickFix @Nullable [] getQuickFixes() {
-    CreatePropertyFix fix = new CreatePropertyFix(myElement, myKey, getPropertiesFiles());
-    return new LocalQuickFix[] {fix};
+  public @NotNull Object[] getVariants() {
+    return PsiReference.EMPTY_ARRAY;
   }
 
+  @NotNull
+  public ResolveResult[] multiResolve(boolean incompleteCode) {
+    PsiElement resolved = resolve();
+    if (resolved != null) {
+      return new ResolveResult[]{new PsiElementResolveResult(resolved)};
+    }
+    return ResolveResult.EMPTY_ARRAY;
+  }
 
+  @NotNull
+  public String getUnresolvedMessagePattern() {
+    return "Cannot resolve property '" + myKey + "'";
+  }
 }
